e517223ea13a0a47c10bbd764b6429b861a64a52
alfredo003 <alfredomanuel127.0@gmail.com>
2024-03-13 21:56:31 +0100

    build the struct of system

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,9 +1,19 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 class User
 {
+    @PrimaryGeneratedColumn()
+    id: number
 
+    @Column({type:'text'})
+    name:string
+
+    @Column({type:'text', unique:true})
+    email:string
+
+    @Column({type:'text'})
+    password:string
 }
 
 export {User};
